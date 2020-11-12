0eed8d390240ac93e354bf90c0f81dafb0d0b720
Wojciech Kalinowski <wojciechkalinowskipoczta@gmail.com>
2020-11-12 21:15:26 +0100

    Secound Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.*;
 
 
 public class Main {
+    static final Logger logger = LogManager.getLogger(Vehicles.class);
 
 public static void main(String args[]){
+
+    logger.info("Vehicle");
+
 
 }
 
--- src/main/java/Vehicles.java
+++ src/main/java/Vehicles.java
@@ -1,2 +1,5 @@
 public class Vehicles {
+
+
+    
 }
